680a6d184207c3628dcad2368a3ee8d3385ba1f7
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-04-03 00:07:36 +0300

    server1 act as a client

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // for close
 
 #include <sys/types.h>
 #include <sys/socket.h>
--- servers.c
+++ servers.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include <stdlib.h>
+#include <unistd.h> // for close
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -36,12 +39,16 @@ int main() {
 
     // 2nd arg - backlog: how many connections can be waiting for this socket.
     // Set 5, but doesn't matter
+
     listen(in_server1_socket, 5);
 
     int in_client1_socket;
     // 2nd param - struct that contains address of the client connection,
     // 3rd - sizeof it. We'll leave it at NULL
+    // DOES NOT PROCEED FURTHER UNTIL IT GETS A CONNECTION
+    printf("here0\n");
     in_client1_socket = accept(in_server1_socket, NULL, NULL);
+    printf("here1\n");
 
     // print the welcoming message
     printf("[%s] %s%s!\n", in_server1_name, welcome_msg, in_server1_name);
@@ -65,10 +72,39 @@ int main() {
 
     /* ----- ACT AS A CLIENT ------- */
 
-    // TODO
+    // create a socket
+    int out_client1_socket;
 
+    // protocol = 0 (default: TCP)
+    out_client1_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // specify an address for the socket
+    struct sockaddr_in out_server1_address;
+    out_server1_address.sin_family = AF_INET;
 
+    // convert integer to network byte order
+    out_server1_address.sin_port = htons(10000);
+
+    // sin_addr - a struct that contains another struct
+    out_server1_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    // cast server_address to different structure
+    int connection_status = connect(out_client1_socket, (struct sockaddr *) &out_server1_address, sizeof(out_server1_address));
+
+    // check for error with the connection
+    // 0 for no errors
+    if (connection_status == -1) {
+        printf("[%s] There was an error making a connection to the remote socket \n\n", in_server1_name);
+    }
+
+    char out_client1_message[256] = "hello";
+    strcpy(out_client1_message, in_client1_message);
+
+    // send data to the server
+    send(out_client1_socket, out_client1_message, sizeof(out_client1_message), 0);
+
+    // and then close the socket
+    close(out_client1_socket);
 
     /* INTERMEDIARY SERVERS ------------------------------------------------------ */
 
@@ -102,14 +138,21 @@ int main() {
     int in_clientL_socket;
     // 2nd param - struct that contains address of the client connection,
     // 3rd - sizeof it. We'll leave it at NULL
+    printf("here21\n");
     in_clientL_socket = accept(in_serverL_socket, NULL, NULL);
+    printf("here22\n");
 
     // print the welcoming message
     printf("[%s] %s%s [Last one]!\n", in_serverL_name, welcome_msg, in_serverL_name);
 
+
+
+
     // receive data from the client
     char in_clientL_message[256];
     recv(in_clientL_socket, &in_clientL_message, sizeof(in_clientL_message), 0);
+
+
 
     // print out the client's msg
     printf("[%s] The received client's message is: %s\n", in_serverL_name, in_clientL_message);
